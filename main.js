4f4896543d14a83fdd899565d948fa5ae0370abd
LautaBorda <lautaro.borda@gmail.com>
2023-01-31 20:47:28 -0300

    finalice la parte de encriptacion

--- main.js
+++ main.js
@@ -11,20 +11,40 @@ let msg = document.getElementById('msg');
 // ENCRYPT FUNCION 
 
 function encryptor() {
-    let encMsg = textInput.value
-    encMsg = textInput.value.replace('a', 'ai')
-    encMsg = textInput.value.replace('e', 'enter')
-    encMsg = textInput.value.replace('i', 'imes')
-    encMsg = textInput.value.replace('o', 'ober')
-    encMsg = textInput.value.replace('u', 'ufat')
+    let encMsg = textInput.value;
+
+    encMsg = encMsg.replaceAll('e', 'enter');
+    encMsg = encMsg.replaceAll('o', 'ober');
+    encMsg = encMsg.replaceAll('i', 'imes');
+    encMsg = encMsg.replaceAll('a', 'ai');
+    encMsg = encMsg.replaceAll('u', 'ufat');
 
     msg.value = encMsg;
 
-    
+}
+
+// DECRYPT FUNCTION
+
+function desencrypter() {
+    let desenMsg = msg.value;
+    desenMsg = desenMsg.replaceAll('enter', 'e')
+    desenMsg = desenMsg.replaceAll('ober', 'o')
+    desenMsg = desenMsg.replaceAll('imes', 'i')
+    desenMsg = desenMsg.replaceAll('ai', 'a')
+    desenMsg = desenMsg.replaceAll('ufat', 'u')
+
+    msg.value=desenMsg;
 
 }
 
+// DELETE FUNCTION 
+
+function dlt(){
+    textInput.value = "";
+}
 
 // EVENT LISTENERS
 
 encryptBtn[0].addEventListener('click', encryptor)
+desencryptBtn[0].addEventListener('click', desencrypter)
+deleteBtn[0].addEventListener('click', dlt)
